a8183f8ebb8276c1396a434804f719895ba647ad
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-13 07:47:08 +0800

    feat(comp): use fetcher in favorite function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import { Form, useLoaderData } from 'react-router-dom';
+import { Form, useLoaderData, useFetcher } from 'react-router-dom';
 import { getContact } from '../modules/utils/contacts';
 
 export async function loader({ params }) {
@@ -65,9 +65,11 @@ export default function Contact() {
 }
 
 function Favorite({ contact }) {
+  const fetcher = useFetcher();
   const favorite = contact.favorite;
+
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? 'false' : 'true'}
@@ -75,6 +77,6 @@ function Favorite({ contact }) {
       >
         {favorite ? '★' : '☆'}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }

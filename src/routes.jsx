c4b26fa20cfeef29bd9deb1d11d39dcdfb2a7ee8
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-12 10:48:41 +0800

    build(routes): add Contact page

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from 'react-router';
 import Root from './components/Root';
 import ErrorPage from './components/ErrorPage';
+import Contact from './components/Contact';
 
 const routes = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: 'contacts/:contactId',
+    element: <Contact />,
   },
 ]);
 

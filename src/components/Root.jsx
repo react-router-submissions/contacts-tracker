95dbb9f41c7f61e969d79e4c05ee4b444b919d34
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-12 12:53:14 +0800

    feat(comp): use react router Form, add create contact async function

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,5 +1,10 @@
-import { Outlet, Link, useLoaderData } from 'react-router';
-import { getContacts } from '../modules/utils/contacts';
+import { Outlet, Link, useLoaderData, Form } from 'react-router';
+import { getContacts, createContact } from '../modules/utils/contacts';
+
+export async function action() {
+  const contact = await createContact();
+  return { contact };
+}
 
 export async function loader() {
   const contacts = await getContacts();
@@ -25,9 +30,9 @@ export default function Root() {
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
           </form>
-          <form method="post">
+          <Form method="post">
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav>
           {contacts.length ? (
